15ca96a0aede427ebaa1e235cfc1dceec735a879
alien2fg <filipgnojek@gmail.com>
2022-10-28 15:42:29 +0200

    Bubblesort

---
+++ 7 kyu Bubblesort Once.cs
@@ -0,0 +1,19 @@
+public class Kata
+{
+    public static int[] BubbleSortOnce(int[] input)
+    {
+        int[] result = new int[input.Length];
+        input.CopyTo(result, 0);
+        int temp=0;
+        for(int i=0;i<result.Length-1;i++)
+          {
+           if (result[i] > result[i+1])
+           {
+           temp = result[i + 1];
+            result[i + 1] = result[i];
+            result[i] = temp;
+           }
+        }
+      return result;
+    }
+}
