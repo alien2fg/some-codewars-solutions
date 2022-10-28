554d82903edf98c7399eb1f6a0d04b8b6b970e73
alien2fg <filipgnojek@gmail.com>
2022-10-28 15:44:43 +0200

    t

---
+++ 8 kyu String repeat.cs
@@ -0,0 +1,17 @@
+namespace Solution
+{
+  using System;
+  using System.Linq;
+  public static class Program
+  {
+    public static string RepeatStr(int n, string s)
+    {
+      string text="";
+      for(int i=0;i<n;i++)
+        {
+        text=text+s;
+        }
+      return text;
+    }
+  }
+}
---
+++ 8 kyu Sum Arrays.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Linq;
+
+public class Kata
+{
+  public static double SumArray(double[] array)
+  {
+    return (array.Length==0) ? 0: array.Sum() ;
+  }
+}
