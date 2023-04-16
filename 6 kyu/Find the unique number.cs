f200bc3bed0a9e758e421f54c8deeb40e1a957c4
Filip Gnojek <94650570+alien2fg@users.noreply.github.com>
2023-04-16 11:32:40 +0200

    Update Find the unique number.cs

--- 6 kyu/Find the unique number.cs
+++ 6 kyu/Find the unique number.cs
@@ -19,3 +19,39 @@ public class Kata
   }
 }
 }
+
+//older version
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+  public static int GetUnique(IEnumerable<int> numbers)
+  {
+    int fElm=numbers.First();
+    int j=0;
+    foreach(int i in numbers){
+        if(i!=fElm){
+          switch(j){
+              case 1:
+                  if(fElm==numbers.ElementAt(2)){
+                    return i;
+                  }
+                   else{
+                     return fElm;
+                  }
+                  break;
+              default:
+                  if(fElm==numbers.ElementAt(j-1)){
+                    return i;
+                  }
+                  else{
+                    return fElm;
+                  }
+          }
+        }
+      j++;
+    }
+    return 0;
+  }
+}
